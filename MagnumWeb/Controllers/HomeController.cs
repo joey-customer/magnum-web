ddec7d76d3ef383b77cddd53862d3aad85ec4a6c
pjamenaja <pjame.fb@gmail.com>
2019-07-30 06:46:15 +0700

    Setup MagnumWeb project

--- MagnumWeb/Controllers/HomeController.cs
+++ MagnumWeb/Controllers/HomeController.cs
@@ -20,6 +20,11 @@ namespace MagnumWeb.Controllers
             return View();
         }
 
+        public IActionResult Seubpong()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
