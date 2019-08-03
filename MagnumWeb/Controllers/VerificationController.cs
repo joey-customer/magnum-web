8bff030f8bd6817d2638ad4df882e46745031620
Abystar <abystar@msn.com>
2019-08-04 03:08:36 +0700

    VerificationController + NUnit

--- MagnumWeb/Controllers/VerificationController.cs
+++ MagnumWeb/Controllers/VerificationController.cs
@@ -3,31 +3,48 @@ using Microsoft.AspNetCore.Mvc;
 using Magnum.Api.Factories;
 using Magnum.Api.Businesses.Registrations;
 using Magnum.Api.Models;
+using Magnum.Api.Commons.Business;
 
 namespace MagnumWeb.Controllers
 {
     public class VerificationController : BaseController
     {
+        private IBusinessOperationManipulate<MRegistration> opr = null;
+
+        public IBusinessOperationManipulate<MRegistration> Opr { get => opr; set => opr = value; }
+
         [HttpGet("verification/{product}/{group}/{serial}/{pin}")]
-        public String Check(String product, String group, String serial, String pin)
+        public IActionResult Check(String product, String group, String serial, String pin)
         {
-            CreateRegistration opr = (CreateRegistration)FactoryBusinessOperation.CreateBusinessOperationObject("CreateRegistration");
+            if (Opr == null)
+            {
+                Opr = (IBusinessOperationManipulate<MRegistration>)FactoryBusinessOperation.CreateBusinessOperationObject("CreateRegistration");
+            }
 
             MRegistration param = new MRegistration();
-            param.IP = Request.HttpContext.Connection.RemoteIpAddress.ToString();
+            param.IP = getRemoteIP();
             param.Pin = pin;
             param.SerialNumber = serial;
             param.Path = string.Format("{0}/{1}", product, group);
 
             try
             {
-                opr.Apply(param);
-                return string.Format("Done register barcode [{0}] [{1}]", serial, pin);
+                Opr.Apply(param);
+                ViewBag.Serial = serial;
+                ViewBag.PIN = pin;
+                return View("Success");
             }
             catch (Exception e)
             {
-                return string.Format("Error : {0}", e.Message);
+                ViewBag.Message = e.Message;
+                return View("Fail");
             }
         }
+
+        public virtual string getRemoteIP()
+        {
+            return Request.HttpContext.Connection.RemoteIpAddress.ToString();
+        }
+
     }
 }
