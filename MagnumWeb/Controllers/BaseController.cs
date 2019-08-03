3637b44dad807713764290d4cccac39a1802465b
Abystar <abystar@msn.com>
2019-08-03 13:35:20 +0700

    Initialize VerificationController

---
+++ MagnumWeb/Controllers/BaseController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MagnumWeb.Models;
+using Magnum.Api.NoSql;
+using Magnum.Api.Factories;
+using Magnum.Api.Businesses.Registrations;
+using Magnum.Api.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace MagnumWeb.Controllers
+{
+    public class BaseController : Controller
+    {
+        private INoSqlContext context = null;
+      
+        public void SetNoSqlContext(INoSqlContext context)
+        {
+            this.context = context;
+        }
+
+        public INoSqlContext GetNoSqlContext()
+        {
+            return context;
+        }
+
+        protected INoSqlContext GetNoSqlContext(string provider, string host, string key, string user, string password)
+        {
+            INoSqlContext ctx = null;
+            if (provider.Equals("firebase"))
+            {
+                ctx = new FirebaseNoSqlContext();
+                ctx.Authenticate(host, key, user, password);
+            }
+
+            return ctx;
+        }
+    }
+}
---
+++ MagnumWeb/Controllers/VerificationController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MagnumWeb.Models;
+using Magnum.Api.NoSql;
+using Magnum.Api.Factories;
+using Magnum.Api.Businesses.Registrations;
+using Magnum.Api.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace MagnumWeb.Controllers
+{
+    public class VerificationController : BaseController
+    {
+        [HttpGet("verification/{product}/{group}/{serial}/{pin}")]
+        public String Check(String product, String group, String serial, String pin)
+        {
+            var view = View();
+
+            INoSqlContext ctx = GetNoSqlContext();
+            if (ctx == null)
+            {
+                string host = "https://compute-engine-vm-test.firebaseio.com/";
+                string key = Environment.GetEnvironmentVariable("MAGNUM_FIREBASE_KEY");
+                string user = Environment.GetEnvironmentVariable("MAGNUM_DB_USERNAME");
+                string password = Environment.GetEnvironmentVariable("MAGNUM_DB_PASSWORD");
+                ctx = GetNoSqlContext("firebase", host, key, user, password);
+                SetNoSqlContext(ctx);
+            }
+
+            FactoryBusinessOperation.SetContext(ctx);
+            CreateRegistration opr = (CreateRegistration)FactoryBusinessOperation.CreateBusinessOperationObject("CreateRegistration");
+
+            MRegistration param = new MRegistration();
+            param.IP = Request.HttpContext.Connection.RemoteIpAddress.ToString();
+            param.Pin = pin;
+            param.SerialNumber = serial;
+            param.Path = string.Format("{0}/{1}", product, group);
+
+            try
+            {
+                opr.Apply(param);
+                return string.Format("Done register barcode [{0}] [{1}]", serial, pin);
+            }
+            catch (Exception e)
+            {
+                return string.Format("Error : {0}", e.Message);
+            }
+        }
+    }
+}
