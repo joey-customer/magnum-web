cc79d93ec2344947ddc7f90cb542b44f916ba842
pjamenaja <pjame.fb@gmail.com>
2019-08-14 20:14:52 +0700

    Added SMTP

--- MagnumCore/Magnum/Api/Smtp/SmtpContextBase.cs
+++ MagnumCore/Magnum/Api/Smtp/SmtpContextBase.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Net.Mail;
+using System.Net;
 
 namespace Magnum.Api.Smtp
 {
@@ -18,8 +20,17 @@ namespace Magnum.Api.Smtp
         }
 
         public void Send(Mail mail)
-        {
-            //Do sending stuff here
+        {                   
+            SmtpClient client = new SmtpClient(smtpHost);
+            client.UseDefaultCredentials = false;
+            client.Credentials = new NetworkCredential(smtpUser, smtpPassword);
+            
+            MailMessage mailMessage = new MailMessage();
+            mailMessage.From = new MailAddress(mail.From);
+            mailMessage.To.Add(mail.To);
+            mailMessage.Body = mail.Body;
+            mailMessage.Subject = mail.Subject;
+            client.Send(mailMessage);
         }
     }    
 }
