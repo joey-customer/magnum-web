632aca773e8f4060f0b0de80b788a73a0cf36725
Abystar <abystar@msn.com>
2019-08-24 17:01:15 +0700

    Fix sonarcloud code smell

--- MagnumCore/Magnum/Api/Businesses/Contents/SaveContent.cs
+++ MagnumCore/Magnum/Api/Businesses/Contents/SaveContent.cs
@@ -4,32 +4,21 @@ using Magnum.Api.Models;
 
 namespace Magnum.Api.Businesses.Contents
 {
-	public class SaveContent : BusinessOperationBase, IBusinessOperationGetInfo<MContent>
-	{
+    public class SaveContent : BusinessOperationBase, IBusinessOperationGetInfo<MContent>
+    {
         public MContent Apply(MContent dat)
         {
             if (!dat.IsKeyIdentifiable())
             {
-                throw(new ArgumentException("Code must not be null!!!"));
+                throw (new ArgumentException("Code must not be null!!!"));
             }
 
-            // GetProductTypeInfo opr = (GetProductTypeInfo)FactoryBusinessOperation.CreateBusinessOperationObject("GetProductTypeInfo");
-            // MContent prd = opr.Apply(dat);
-
             var ctx = GetNoSqlContext();
-             
-            // if (prd == null)
-            // {
-                //Does not exist then create new one
-                string path = string.Format("contents/{0}", dat.Type + "_" + dat.Name);                 
-                //Put again to eliminate the GUI_ID key
-                ctx.PutData(path, "", dat);  
-            // }
-            // else
-            // {
-            //     dat.Key = prd.Key;
-            //     ctx.PutData("contents", dat.Code, dat);  
-            // }        
+
+            //Does not exist then create new one
+            string path = string.Format("contents/{0}", dat.Type + "_" + dat.Name);
+            //Put again to eliminate the GUI_ID key
+            ctx.PutData(path, "", dat);
 
             //Return null indicates create new one, not null indicates update the existing one            
             return null;
