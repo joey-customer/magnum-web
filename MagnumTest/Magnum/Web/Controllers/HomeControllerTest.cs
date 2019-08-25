daedb151cb5b8a2a305b4609621a7dfa8b6cc269
Abystar <abystar@msn.com>
2019-08-26 01:36:46 +0700

    Separate ContactUs from HomeController

--- MagnumTest/Magnum/Web/Controllers/HomeControllerTest.cs
+++ MagnumTest/Magnum/Web/Controllers/HomeControllerTest.cs
@@ -1,12 +1,7 @@
-using System;
 using NUnit.Framework;
 using Moq;
-using Magnum.Api.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
-using System.Net;
-using Magnum.Api.Commons.Business;
-using Magnum.Api.Businesses.ContactUs;
 using Magnum.Web.Models;
 using Magnum.Api.Caches;
 
@@ -15,8 +10,6 @@ namespace Magnum.Web.Controllers
     public class HomeControllerTest
     {
         HomeController controller;
-        IBusinessOperationManipulate<MContactUs> mockContactUsOpr;
-
 
         [SetUp]
         public void Setup()
@@ -26,14 +19,9 @@ namespace Magnum.Web.Controllers
             {
                 HttpContext = httpContext,
             };
-            controllerContext.HttpContext.Connection.RemoteIpAddress = IPAddress.Parse("127.0.0.1");
 
             var mockController = new Mock<HomeController>() { CallBase = true };
-            var mockOpr = new Mock<IBusinessOperationManipulate<MContactUs>>();
-            mockContactUsOpr = mockOpr.Object;
-            mockOpr.Setup(foo => foo.Apply(It.IsAny<MContactUs>())).Returns(0);
 
-            mockController.Setup(foo => foo.GetSaveContactUsOperation()).Returns(mockContactUsOpr);
             var iCacheMock = new Mock<ICache>();
             mockController.Setup(foo => foo.GetContentCache()).Returns(iCacheMock.Object);
 
@@ -72,72 +60,11 @@ namespace Magnum.Web.Controllers
         }
 
         [Test]
-        public void ContactTest()
-        {
-            ViewResult result = (ViewResult)controller.Contact();
-            Assert.IsNotNull(result);
-        }
-
-        [Test]
         public void ErrorTest()
         {
             ViewResult result = (ViewResult)controller.Error();
             ErrorViewModel evm = (ErrorViewModel)result.Model;
             Assert.IsNotNull(evm);
-        }
-
-        [TestCase("", "A", "B", "C", "Name cannot be empty.")]
-        [TestCase("A", "", "B", "C", "Subject cannot be empty.")]
-        [TestCase("A", "B", "", "C", "Email cannot be empty.")]
-        [TestCase("A", "B", "C", "", "Message cannot be empty.")]
-        [TestCase("A", "B", "C", "D", null)]
-        public void ValidateContactUsFormTest(String name, String subject, String email, String message, string expected)
-        {
-            MContactUs model = new MContactUs();
-            model.Name = name;
-            model.Subject = subject;
-            model.Email = email;
-            model.Message = message;
-            string result = controller.ValidateContactUsForm(model);
-            Assert.AreEqual(expected, result);
-        }
-
-        [TestCase("Maxnum", "0000", "1234", "5678")]
-        public void SaveContactTest(String name, String subject, String email, String message)
-        {
-            MContactUs model = new MContactUs();
-            model.Name = name;
-            model.Subject = subject;
-            model.Email = email;
-            model.Message = message;
-            ViewResult result = (ViewResult)controller.SaveContactUs(model);
-            Assert.AreEqual("127.0.0.1", model.IP);
-            Assert.AreEqual("Contact", result.ViewName);
-            Assert.AreEqual("Thank you for contacting us – we will get back to you soon!", result.ViewData["Message"]);
-        }
-
-        [TestCase("", "0000", "1234", "5678")]
-        public void SaveContactTestEmpty(String name, String subject, String email, String message)
-        {
-            MContactUs model = new MContactUs();
-            model.Name = name;
-            model.Subject = subject;
-            model.Email = email;
-            model.Message = message;
-            ViewResult result = (ViewResult)controller.SaveContactUs(model);
-            Assert.AreEqual(null, model.IP);
-            Assert.AreEqual("Contact", result.ViewName);
-            Assert.AreEqual("Name cannot be empty.", result.ViewData["Message"]);
-        }
-
-        [Test]
-        public void GetSaveContactUsOperationTest()
-        {
-            var mockController = new Mock<HomeController>() { CallBase = true };
-            controller = mockController.Object;
-
-            SaveContactUs result = (SaveContactUs)controller.GetSaveContactUsOperation();
-            Assert.IsNotNull(result);
         }
 
         [Test]
--- MagnumWeb/Magnum/Web/Controllers/HomeController.cs
+++ MagnumWeb/Magnum/Web/Controllers/HomeController.cs
@@ -19,7 +19,6 @@ namespace Magnum.Web.Controllers
             return FactoryCache.GetCacheObject("CachePageContents");
         }
 
-
         public IActionResult Index()
         {
             var contentCache = GetContentCache();
@@ -45,66 +44,10 @@ namespace Magnum.Web.Controllers
             return View();
         }
 
-        public IActionResult Contact()
-        {
-            return View();
-        }
-
-        [HttpPost("Home/Contact/Save")]
-        public IActionResult SaveContactUs(MContactUs form)
-        {
-            string validationMessage = ValidateContactUsForm(form);
-            if (ValidateContactUsForm(form) != null)
-            {
-                ViewBag.Message = validationMessage;
-            }
-            else
-            {
-                IBusinessOperationManipulate<MContactUs> operation = GetSaveContactUsOperation();
-                form.IP = RemoteUtils.GetRemoteIPAddress(ControllerContext);
-                form.Name = StringUtils.StripTagsRegex(form.Name);
-                form.Subject = StringUtils.StripTagsRegex(form.Subject);
-                form.Email = StringUtils.StripTagsRegex(form.Email);
-                form.Message = StringUtils.StripTagsRegex(form.Message);
-
-                operation.Apply(form);
-
-                ViewBag.Message = "Thank you for contacting us – we will get back to you soon!";
-            }
-            return View("Contact");
-        }
-
-        public string ValidateContactUsForm(MContactUs form)
-        {
-            string validationMessage = null;
-            if (String.IsNullOrEmpty(form.Name))
-            {
-                validationMessage = "Name cannot be empty.";
-            }
-            else if (String.IsNullOrEmpty(form.Subject))
-            {
-                validationMessage = "Subject cannot be empty.";
-            }
-            else if (String.IsNullOrEmpty(form.Message))
-            {
-                validationMessage = "Message cannot be empty.";
-            }
-            else if (String.IsNullOrEmpty(form.Email))
-            {
-                validationMessage = "Email cannot be empty.";
-            }
-            return validationMessage;
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
-        public virtual IBusinessOperationManipulate<MContactUs> GetSaveContactUsOperation()
-        {
-            return (IBusinessOperationManipulate<MContactUs>)FactoryBusinessOperation.CreateBusinessOperationObject("SaveContactUs");
         }
     }
 }
