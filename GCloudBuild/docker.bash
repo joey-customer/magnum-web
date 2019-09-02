56a2e6e8db4fb3711512ac572bf2e3df17b30c9f
pjamenaja <pjame.fb@gmail.com>
2019-09-02 21:18:59 +0700

    Test Cloud Build

--- GCloudBuild/docker.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd /workspace
-
-PACAKGE=pjamenaja/magnum-web:${DOCKER_VERSION}
-
-docker build --build-arg VERSION_NUMBER=${BUILT_VERSION} -f /workspace/GCloudBuild/Dockerfile -t ${PACAKGE} /workspace
-#docker login -u ${DOCKER_HUB_USERNAME} -p ${DOCKER_HUB_PASSWORD} docker.io
-#docker push ${PACAKGE}
