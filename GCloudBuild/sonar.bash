7c5fdfcc6ac9c83a4d0bc5b238053dbd2f9387b1
pjamenaja <pjame.fb@gmail.com>
2019-09-02 11:14:56 +0700

    Test Cloud Build

--- GCloudBuild/sonar.bash
+++ GCloudBuild/sonar.bash
@@ -2,13 +2,17 @@
 
 #Be careful to set this file without \r
 
+cd /workspace
+
 dotnet sonarscanner begin \
     /key:pjamenaja_magnum_web \
     /o:pjamenaja \
+    /v:${BUILT_VERSION} \
     /d:sonar.host.url=https://sonarcloud.io \
+    /d:sonar.branch.name=${BRANCH_NAME} \
+    /d:sonar.cs.opencover.reportsPaths=./coverage.opencover.xml \
+    /d:sonar.javascript.exclusions=**/bootstrap/**,**/jquery/**,**/jquery-validation/**,**/jquery-validation-unobtrusive/** \
     /d:sonar.login=${SONAR_KEY}
-
-cd /workspace
 
 dotnet build Magnum.sln
 
