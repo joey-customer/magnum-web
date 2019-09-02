22c628f677d43971ecac757f30cae78efa8561ed
pjamenaja <pjame.fb@gmail.com>
2019-09-02 10:07:12 +0700

    Test Cloud Build

--- sonar.bash
+++ sonar.bash
@@ -2,5 +2,5 @@
 
 dotnet sonarscanner begin /key:pjamenaja_magnum_web /o:pjamenaja /d:sonar.host.url=https://sonarcloud.io /d:sonar.login=6b3024e6cc1576028a5bba761b1009ffb850b6f5
 cd /workspace
-dotnet build /workspace/Magnum.sln
+dotnet build Magnum.sln
 dotnet sonarscanner end /d:sonar.login=6b3024e6cc1576028a5bba761b1009ffb850b6f5
