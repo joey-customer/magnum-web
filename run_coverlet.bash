fd45aad439c6556b3d557cffba10655656a608d3
Abystar <abystar@msn.com>
2019-09-08 11:01:29 +0700

    Improve run_coverlet script performance by removing the duplicate test.

--- run_coverlet.bash
+++ run_coverlet.bash
@@ -1,3 +1,2 @@
 #!/bin/bash
-
-dotnet test ; coverlet ./MagnumTest/bin/Debug/netcoreapp2.2/MagnumTest.dll --target "dotnet" --targetargs "test . --no-build" --format lcov
+coverlet ./MagnumTest/bin/Debug/netcoreapp2.2/MagnumTest.dll --target "dotnet" --targetargs "test . --no-build" --format lcov
