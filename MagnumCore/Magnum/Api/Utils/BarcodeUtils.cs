f0bc0b33bc602d90e82f2cf5a978142e1aefae7a
Abystar <abystar@msn.com>
2019-09-08 16:03:19 +0700

    Merge duplicated codes

---
+++ MagnumCore/Magnum/Api/Utils/BarcodeUtils.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class BarcodeUtils
+{
+    internal static string BuildBarcodePath(string tableName, string strSerial, string strPin)
+    {
+        char[] serialNumber = strSerial.ToCharArray();
+        char[] pin = strPin.ToCharArray();
+        return String.Format("{0}/{1}/{2}/{3}/{4}/{5}/{6}/{7}/{8}", tableName
+                    , serialNumber[0], serialNumber[1], serialNumber[2]
+                    , pin[0], pin[1], pin[2], strSerial, strPin);
+    }
+}
