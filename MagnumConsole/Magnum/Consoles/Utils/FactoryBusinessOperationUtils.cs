3c4dfc95608a0f11f971a3f56aea64fda0c0a7fd
Abystar <abystar@msn.com>
2019-11-02 14:08:51 +0700

    Fix code smell

--- MagnumConsole/Magnum/Consoles/Utils/FactoryBusinessOperationUtils.cs
+++ MagnumConsole/Magnum/Consoles/Utils/FactoryBusinessOperationUtils.cs
@@ -1,16 +1,14 @@
-using System;
-
 using Its.Onix.Core.Factories;
 using Its.Onix.Erp.Services;
 
 namespace Magnum.Consoles.Utils
 {
-	public static class FactoryBusinessOperationUtils
-	{
+    public static class FactoryBusinessOperationUtils
+    {
         private static bool isLoad = false;
 
         public static void LoadBusinessOperations()
-        {   
+        {
             if (!isLoad)
             {
                 FactoryBusinessOperation.RegisterBusinessOperations(BusinessErpOperations.GetInstance().ExportedServicesList());
